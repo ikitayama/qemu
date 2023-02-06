52772fa622835be5ba4ea37cabc225526fa8f69b
Jean-Philippe Brucker <jean-philippe@linaro.org>
2023-02-06 16:49:25 +0000

    hw/arm/virt: Add support for Arm RME
    When confidential-guest-support is enabled for the virt machine, call
    the RME init function, and add the RME flag to the VM type.
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- hw/arm/virt.c
+++ hw/arm/virt.c
@@ -224,6 +224,11 @@ static const char *valid_cpus[] = {
     ARM_CPU_TYPE_NAME("max"),
 };
 
+static bool virt_machine_is_confidential(VirtMachineState *vms)
+{
+    return MACHINE(vms)->cgs;
+}
+
 static bool cpu_type_valid(const char *cpu)
 {
     int i;
@@ -2100,10 +2105,11 @@ static void machvirt_init(MachineState *machine)
      * if the guest has EL2 then we will use SMC as the conduit,
      * and otherwise we will use HVC (for backwards compatibility and
      * because if we're using KVM then we must use HVC).
+     * Realm guests must also use SMC.
      */
     if (vms->secure && firmware_loaded) {
         vms->psci_conduit = QEMU_PSCI_CONDUIT_DISABLED;
-    } else if (vms->virt) {
+    } else if (vms->virt || virt_machine_is_confidential(vms)) {
         vms->psci_conduit = QEMU_PSCI_CONDUIT_SMC;
     } else {
         vms->psci_conduit = QEMU_PSCI_CONDUIT_HVC;
@@ -2153,6 +2159,10 @@ static void machvirt_init(MachineState *machine)
                      "MTE to the guest CPU",
                      current_accel_name());
         exit(1);
+    }
+
+    if (machine->cgs && kvm_enabled()) {
+        kvm_arm_rme_init(machine->cgs, &error_fatal);
     }
 
     create_fdt(vms);
@@ -2901,6 +2911,7 @@ static HotplugHandler *virt_machine_get_hotplug_handler(MachineState *machine,
 static int virt_kvm_type(MachineState *ms, const char *type_str)
 {
     VirtMachineState *vms = VIRT_MACHINE(ms);
+    int rme_vm_type = kvm_arm_rme_vm_type(ms);
     int max_vm_pa_size, requested_pa_size;
     bool fixed_ipa;
 
@@ -2930,7 +2941,11 @@ static int virt_kvm_type(MachineState *ms, const char *type_str)
      * the implicit legacy 40b IPA setting, in which case the kvm_type
      * must be 0.
      */
-    return fixed_ipa ? 0 : requested_pa_size;
+    if (fixed_ipa) {
+        return 0;
+    }
+
+    return requested_pa_size | rme_vm_type;
 }
 
 static void virt_machine_class_init(ObjectClass *oc, void *data)
