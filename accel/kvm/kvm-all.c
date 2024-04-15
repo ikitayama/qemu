56d36db5abae2e9fdfb63e67c4d5c4bcacc812aa
Jean-Philippe Brucker <jean-philippe@linaro.org>
2024-04-15 19:00:04 +0100

    HACK: kvm: Workaround CCA unmap-before-RD issue
    Currently the KVM support for CCA reacts badly to changing guest memory
    state before a RD is created. Until this is fixed in the Linux branch,
    skip the initial memory state change. That's OK to do on CCA because the
    guest needs to initialize the RIPAS before accessing RAM, causing a
    bunch of returns to QEMU which call set_memory_attributes_private().
    
    Signed-off-by: Jean-Philippe Brucker <jean-philippe@linaro.org>

--- accel/kvm/kvm-all.c
+++ accel/kvm/kvm-all.c
@@ -1422,7 +1422,7 @@ static void kvm_set_phys_mem(KVMMemoryListener *kml,
             abort();
         }
 
-        if (memory_region_has_guest_memfd(mr)) {
+        if (0 && memory_region_has_guest_memfd(mr)) {
             err = kvm_set_memory_attributes_private(start_addr, slot_size);
             if (err) {
                 error_report("%s: failed to set memory attribute private: %s\n",
